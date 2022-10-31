01f09c71e40b700764472d05cc7eb3c2fb99820a
AjLucelAtok <116326156+AjLucelAtok@users.noreply.github.com>
2022-10-31 22:12:59 +0800

    Update BicycleJava.java

--- BicycleJava.java
+++ BicycleJava.java
@@ -1,3 +1,23 @@
 public class BicycleJava
-{
+{private String ownerName;
+	private String tagNo;
+
+	public BicycleJava() {
+		ownerName = "Unknown";
+		tagNo = "Unkown";
+	}
+	public String getOwnerName() {
+		return ownerName;
+	}
+	public void setOwnerName(String name){
+		ownerName = name;
+	}
+	public void settagNo(String num){
+		tagNo = num;
+	}
+	public  String gettagNo(){
+		return tagNo;
+	}
+	
+
 }
